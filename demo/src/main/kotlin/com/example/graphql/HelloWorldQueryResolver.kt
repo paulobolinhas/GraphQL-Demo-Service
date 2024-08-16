c457eaa53e3a2043bd9b913692c889bb5be5b099
Paulo Bolinhas <91274978+paulobolinhas@users.noreply.github.com>
2024-08-16 16:58:04 +0100

    Delete demo directory

--- demo/src/main/kotlin/com/example/graphql/HelloWorldQueryResolver.kt
+++
@@ -1,9 +0,0 @@
-package com.example.graphql
-
-import com.expediagroup.graphql.server.operations.Query
-import org.springframework.stereotype.Component
-
-@Component
-class HelloWorldQueryResolver : Query {
-    fun hello(): String = "Hello, World!"
-}
