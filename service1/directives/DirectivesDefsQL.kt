eb6c34a40e142d9915dd0d45596a916ee2a7d667
Paulo Bolinhas <91274978+paulobolinhas@users.noreply.github.com>
2024-08-16 16:45:46 +0100

    Add files via upload

---
+++ service1/directives/DirectivesDefsQL.kt
@@ -0,0 +1,12 @@
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Uppercase
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Trim
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class SplitMarket
+
